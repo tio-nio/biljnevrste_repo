58ea5cf09f49f2b39a75ebca0e7e069eb1cee241
tio-nio <antonio@bodul.com>
2019-05-23 11:33:40 +0200

    frontend

---
+++ projekt/frontend/src/pages/indeks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 1600px">
+      <q-tabs
+        v-model="tab"
+        class="bg-primary text-white shadow-2"
+      >
+        <q-tab name="unos" label="Unos Biljaka">
+          <q-badge color="red" floating></q-badge>
+        </q-tab>
+        <q-tab name="pregled" label="Pregled Biljaka">
+          <q-badge color="red" floating></q-badge>
+        </q-tab>
+        <q-tab name="kviz" label="Pregled rezultata kviza" />
+      </q-tabs>
+    </div>
+  </div>
+</template>
---
+++ projekt/frontend/src/router/routes.js
@@ -0,0 +1,26 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') }
+    ]
+  },
+  {
+    path: '/indeks',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Indeks.vue') }
+    ]
+  }
+]
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
